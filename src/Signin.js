fdb163e44a8c1133af6b737a9b94eab9cbbc6478
duyduc1 <106147757+duyduc1@users.noreply.github.com>
2024-01-14 12:33:18 +0700

    clientweb

---
+++ src/Signin.js
@@ -0,0 +1,68 @@
+import React from "react";
+import Axios from "axios"
+
+function SignInForm() {
+
+  const [state, setState] = React.useState({
+    name: "",
+    password: ""
+  });
+  const handleChange = evt => {
+    const value = evt.target.value;
+    setState({
+      ...state,
+      [evt.target.name]: value
+    });
+  };
+
+  const handleOnSubmit = evt => {
+    evt.preventDefault();
+
+    const { name, password } = state;
+    alert(`You are login with username: ${name} and password: ${password}`);
+
+    for (const key in state) {
+      setState({
+        ...state,
+        [key]: ""
+      });
+    }
+  };
+
+  return (
+    <div className="form-container sign-in-container">
+      <form onSubmit={handleOnSubmit}>
+        <h1>Sign in</h1>
+        <div className="social-container">
+          <a href="#" className="social">
+            <i className="fab fa-facebook-f" />
+          </a>
+          <a href="#" className="social">
+            <i className="fab fa-google-plus-g" />
+          </a>
+          <a href="#" className="social">
+            <i className="fab fa-linkedin-in" />
+          </a>
+        </div>
+        <span>or use your account</span>
+        <input
+          type="text"
+          placeholder="Username"
+          name="text"
+          value={state.name}
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Password"
+          value={state.password}
+          onChange={handleChange}
+        />
+        <a href="#">Forgot your password?</a>
+        <button>Sign In</button>
+      </form>
+    </div>
+  );
+}
+export default SignInForm;
